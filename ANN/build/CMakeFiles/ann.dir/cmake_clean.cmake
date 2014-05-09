bbe62537db070c1eccb1de5e6d982740a5482563
wqzhang <zhangweiqi357@gmail.com>
2014-05-10 01:27:53 +0800

    draft

---
+++ ANN/build/CMakeFiles/ann.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ann.dir/main.cpp.o"
+  "ann.pdb"
+  "ann"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ann.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ANN/image.h
@@ -0,0 +1,10 @@
+#ifndef _IMAGE_H_
+#define _IMAGE_H_
+
+struct IMAGE{
+	int row,col;
+	double *data;
+	double *target;
+};
+
+#endif
